2f9858abba7adbaf4436f8e623cd16510b77b347
AJ <alfcabrales@gmail.com>
2026-06-04 17:26:55 -0600

    Arrays lab completed

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,28 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+// Lof first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+// Add a new product
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// Update a product name at a specific index
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  }
+}
+
+
+// Remove the last product
+function removeLastProduct() {
+  products.pop();
+}
 
 
 // Export the necessary parts for testing
